0d70962efba1b9b01626fd7bfc8421b8a0a1dfd0
yutwatan <yutwatan@yahoo.co.jp>
2020-05-04 20:33:55 +0900

    create controller and entity

--- README.md
+++ README.md
@@ -1,7 +1,35 @@
-# Awesome Project Build with TypeORM
+# numbers-backend
 
 Steps to run this project:
 
 1. Run `npm i` command
 2. Setup database settings inside `ormconfig.json` file
 3. Run `npm start` command
+
+## 作成方法
+
+テンプレート作成
+```
+typeorm init --name numbers-backend --database mysql
+```
+
+mysql2 を使う
+```shell script
+npm install --save mysql2
+npm uninstall --save mysql
+```
+
+Express インストール
+```
+npm install --save express body-parser
+```
+
+ESLint と Prettier インストール
+```shell script
+npm install -D eslint prettier eslint-plugin-prettier eslint-config-prettier @typescript-eslint/parser @typescript-eslint/eslint-plugin
+```
+
+.eslintrc.json 作成
+```shell script
+vim .eslintrc.json
+```
---
+++ src/controller/EventController.ts
@@ -0,0 +1,40 @@
+import { Between, FindManyOptions, getRepository } from 'typeorm';
+import { Request, Response } from 'express';
+import { Event } from '../entity/Event';
+
+export async function eventGetAll(req: Request, res: Response): Promise<void> {
+  const eventRepository = getRepository(Event);
+  const options: FindManyOptions = {
+    relations: ['number'],
+    order: {
+      times: 'ASC',
+    },
+  };
+
+  if (
+    Object.prototype.hasOwnProperty.call(req.query, 'from') &&
+    Object.prototype.hasOwnProperty.call(req.query, 'to')
+  ) {
+    options.where = {
+      date: Between(req.query.from, req.query.to),
+    };
+  }
+
+  const eventData = await eventRepository.find(options);
+  res.send(eventData);
+}
+
+export async function eventGetById(req: Request, res: Response): Promise<void> {
+  const eventRepository = getRepository(Event);
+  const eventData = await eventRepository.findOne(req.params.id, {
+    relations: ['number'],
+  });
+
+  if (!eventData) {
+    res.status(404);
+    res.end();
+    return;
+  }
+
+  res.send(eventData);
+}
